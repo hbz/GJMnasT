ef4e96bbe30000b1f2faddc154f1b460714a2ae8
Andres Quast <aquast@chasmops.fritz.box>
2026-06-07 11:36:03 +0200

    added example class

---
+++ src/main/java/de/nrw/hbz/gjmnast/console/AppStarter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package de.nrw.hbz.gjmnast.console;
+
+/**
+ * 
+ */
+public class AppStarter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
